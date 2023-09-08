200f727ac928c9c9a5ab8095c4edde7734d4ae2d
pavan kumar <141198567+pa1bandaru@users.noreply.github.com>
2023-09-08 16:54:14 +0530

    tst

--- First/Arrays/Java Array/array2.java
+++ First/Arrays/Java Array/array2.java
@@ -1,6 +1,6 @@
 class Test{
     public static void main(String[] args){
-        cal_Marks(new int[]{35,36,37,38,39,40});
+        cal_Marks(new int[]{35,36,37,38,39,40,41,99});
     }
     public static void cal_Marks(int[] x){
                 int total = 0;
---
+++ First/Arrays/Java Array/demo.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String[] args){
+        int marks[] = {1,2,3,4,5,6,7,8,88,99,101};
+        String enames = "Bandaru Pavan Kumar";
+        System.out.println(marks.length);
+        System.out.println(enames.length());
+    }
+}
